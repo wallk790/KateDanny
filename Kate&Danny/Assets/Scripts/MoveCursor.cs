26eb6515670f4bbad5cf1a0cbaf15491a09c93a0
Danny <shortieofcheese@gmail.com>
2015-11-07 22:49:50 -0500

    Puzzle Interaction Scene

---
+++ Kate&Danny/Assets/Scripts/MoveCursor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCursor : MonoBehaviour {
+
+
+	
+	public float speed;
+	public bool fakeClick;
+
+	void Update () {
+
+		Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+		transform.position += move * speed * Time.deltaTime;
+	
+		if(Input.GetButtonDown("AButton")){
+			// simulate a mouse press
+			//parent other object to this transform
+		}
+	}
+}
