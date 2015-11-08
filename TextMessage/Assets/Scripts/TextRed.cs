b08b8c85c25727527215cd5bb912c10ecdf5283f
Wallk790walclear <wallk790@newschool.edu>
2015-11-07 23:10:10 -0500

    TextMessages
    Day One BAMN!

---
+++ TextMessage/Assets/Scripts/TextRed.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextRed : MonoBehaviour {
+
+	public GameObject [] text1; 
+	public Transform [] TextMessages;
+	//public float messagePosition;
+	private int currentText = 0;
+	private int currentPosition = 0;
+	
+	// Use this for initialization
+	void Start () {
+		//text1 = GameObject.FindGameObjectsWithTag ("message");
+		
+		//	messagePosition = 50.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		//if (Input.GetMouseButtonDown(0)) {
+		if (Input.GetMouseButtonDown(1)){
+			//			for(int i = 0; i < text1.Length; i++) {
+			//			Instantiate(text1[i], TextMessages.position, TextMessages.rotation);
+			//			Debug.Log ("text appeared");
+			instantiateNextText();
+		}
+	}
+	
+	void instantiateNextText() {
+		Instantiate(text1[currentText], TextMessages[currentPosition].transform.position, TextMessages[currentPosition].transform.rotation);
+		currentText++;
+		currentPosition++;
+		currentText = currentText % text1.Length;
+		currentPosition = currentPosition % TextMessages.Length;
+		
+	}
+}
